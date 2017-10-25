141c3b221da958a9c91bac25bc82e5199f8eaaf4
rishiwhite11 <rishiwhite@gmail.com>
2017-10-25 10:11:54 +0530

    Added spring cloud configuration

---
+++ src/main/java/com/fidelity/config/ConfigServerLocalApplication.java
@@ -0,0 +1,14 @@
+package com.fidelity.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerLocalApplication.class, args);
+	}
+}
